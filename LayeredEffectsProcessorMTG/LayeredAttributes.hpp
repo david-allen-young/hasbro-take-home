10c6c764626c5ece444896c77964fb418e27ee56
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-02-11 09:17:47 -0800

    replace minHeap with map
    Decided that a sorted map keyed to layer holding a std::vector<Effect> would be better than the priority queue because there is no guarantee against new effects being added after a GetCurrentAttribute() and it would inefficient to empty the minHeap only to have to fill it again each time the result is recalculated

--- LayeredEffectsProcessorMTG/LayeredAttributes.hpp
+++ LayeredEffectsProcessorMTG/LayeredAttributes.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "ILayeredAttributes.hpp"
-#include <queue>
+//#include <queue>
+#include <vector>
+#include <map>
 #include <array>
 
 class LayeredAttributes : public ILayeredAttributes
@@ -17,15 +19,16 @@ private:
 	std::array<int, NumAttributes> baseAttributes;
 	std::array<int, NumAttributes> currentAttributes;
 	using Effect = LayeredEffectDefinition;
-	struct EffectCmp
-	{
-		bool operator()(std::pair<Effect, int> lhs, std::pair<Effect, int> rhs) const
-		{
-			bool sameLayer = (lhs.first.Layer == rhs.first.Layer);
-			bool timestampPriority = (lhs.second > rhs.second);
-			bool layerPriority = (lhs.first.Layer > rhs.first.Layer);
-			return sameLayer ? timestampPriority : layerPriority;
-		}
-	};
-	std::priority_queue<std::pair<Effect, int>, std::vector<std::pair<Effect, int>>, EffectCmp> minHeap;
+	//struct EffectCmp
+	//{
+	//	bool operator()(std::pair<Effect, int> lhs, std::pair<Effect, int> rhs) const
+	//	{
+	//		bool sameLayer = (lhs.first.Layer == rhs.first.Layer);
+	//		bool timestampPriority = (lhs.second > rhs.second);
+	//		bool layerPriority = (lhs.first.Layer > rhs.first.Layer);
+	//		return sameLayer ? timestampPriority : layerPriority;
+	//	}
+	//};
+	//std::priority_queue<std::pair<Effect, int>, std::vector<std::pair<Effect, int>>, EffectCmp> minHeap;
+	std::map<int, std::vector<Effect>> layeredEffects;
 };
