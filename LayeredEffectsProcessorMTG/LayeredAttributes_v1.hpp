a632e40a5f71e62f4be44f326cfc34191fecc62f
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-02-12 02:58:26 -0800

    Create LayeredAttributes_v1.hpp

---
+++ LayeredEffectsProcessorMTG/LayeredAttributes_v1.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "ILayeredAttributes.hpp"
+#include <vector>
+#include <map>
+#include <array>
+
+class LayeredAttributes : public ILayeredAttributes
+{
+public:
+	LayeredAttributes(bool errorLoggingEnabled = false, bool errorHandlingEnabled = false, size_t rereservationSize = 10ULL);
+	virtual ~LayeredAttributes() = default;
+	void SetBaseAttribute(AttributeKey attribute, int value) override;
+    int GetCurrentAttribute(AttributeKey attribute) const override;
+    void AddLayeredEffect(LayeredEffectDefinition effect) override;
+    void ClearLayeredEffects() override;
+
+private:
+	bool errorLoggingEnabled;
+	bool errorHandlingEnabled;
+	size_t reservationSize;
+
+	static const size_t NumAttributes = AttributeKey::AttributeKey_Controller + 1;
+	std::array<int, NumAttributes> baseAttributes;
+	std::array<int, NumAttributes> currentAttributes;
+	int highestLayer = std::numeric_limits<int>::min();
+
+	// NB: I've chosen a (sorted) map over the (more efficient) unordered_map
+	// because I believe the total number of layeredEffects inserted
+	// will likely be less than 100 and it is easier to keep the
+	// effects sorted by layer during insertion since I am iterating
+	// through the map during recalculateCurrentAttributes()
+	std::map<int, std::vector<LayeredEffectDefinition>> layeredEffects;
+
+	void recalculateCurrentAttributes();
+	void updateCurrentAttributes(const LayeredEffectDefinition& effect);
+	void logError(LayeredEffectDefinition effect);
+	void logError(AttributeKey attribute) const;
+	bool attributeInBounds(AttributeKey attribute) const;
+};
