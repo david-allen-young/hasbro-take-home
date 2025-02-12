2c48333b16afa6ca4bdeffc8a624db316b6ed691
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-02-11 16:38:28 -0800

    finished implementing testComplexAdd()

--- LayeredEffectsProcessorMTG/LayeredAttributesUnitTests.hpp
+++ LayeredEffectsProcessorMTG/LayeredAttributesUnitTests.hpp
@@ -105,9 +105,9 @@ private:
 		* AddLayeredEffect(Power, Add, 1, 2) calls updateCurrentAttributes() and results in currentAttributes[Power] == 2 + 1 == 3
 		* AddLayeredEffect(Toughness, Add, 1, 2) calls updateCurrentAttributes() and results in currentAttributes[Toughness] == 1 + 1 == 2
 		* AddLayeredEffect(Power, Set, 4, 0) calls recalculateCurrentAttributes() and results in [Power] == 4 + 1 == 5
-		* AddLayeredEffect(Power, Multiply, 2, 1) calls "eek a BUG!!!"
-		* 
-		* 
+		* AddLayeredEffect(Power, Multiply, 2, 1) calls recalculateCurrentAttributes() and results in [Power] == (4 * 2) + 1 == 9
+		* AddLayeredEffect(Power, Subtract, 1, 2) call updateCurrentAttributes() and results in [Power] == 9 - 1 == 8
+		* At this point Power should be 8 and Toughness should be 2
 		*/
 		std::vector<LayeredEffectDefinition> effects;
 		effects.push_back({ AttributeKey_Power, EffectOperation_Add, /*modifier*/1, /*layer*/2 });
@@ -115,10 +115,12 @@ private:
 		effects.push_back({ AttributeKey_Power, EffectOperation_Set, /*modifier*/basePower + 2, /*layer*/0 });
 		effects.push_back({ AttributeKey_Power, EffectOperation_Multiply, /*modifier*/2, /*layer*/1 });
 		effects.push_back({ AttributeKey_Power, EffectOperation_Subtract, /*modifier*/1, /*layer*/2 });
-
 		for (auto& effect : effects)
 		{
 			attributes.AddLayeredEffect(effect);
 		}
+		assert(attributes.GetCurrentAttribute(AttributeKey::AttributeKey_Power) == 8);
+		assert(attributes.GetCurrentAttribute(AttributeKey::AttributeKey_Toughness) == 2);
+		std::cout << "testComplexAdd passed" << std::endl;
 	}
 };
