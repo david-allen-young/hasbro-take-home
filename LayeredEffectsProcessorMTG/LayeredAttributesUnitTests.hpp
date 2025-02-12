2aa52930a32047358de724208d2658c4214eb58e
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-02-11 16:21:00 -0800

    WIP implement testComplexAdd()

--- LayeredEffectsProcessorMTG/LayeredAttributesUnitTests.hpp
+++ LayeredEffectsProcessorMTG/LayeredAttributesUnitTests.hpp
@@ -87,6 +87,38 @@ private:
 	}
 	void testComplexAdd()
 	{
-		// TODO: add several effects with at least one layer vs. timestamp conflict to ensure recalculation works right
+		LayeredAttributes attributes;
+		int basePower = 2;
+		int baseToughness = 1;
+		/*
+		* Walkthrough:
+		* SetBaseAttribute() results in baseAttributes[Power] == 2; recalculateCurrentAttributes() is called but layeredEffects are empty
+		* SetBaseAttribute() results in baseAttributes[Toughness] == 1; recalculateCurrentAttributes() is called but layeredEffects are empty
+		* At this point baseAttributes and currentAttributes should be the same with Power 2 and Toughness 1
+		*/
+		attributes.SetBaseAttribute(AttributeKey::AttributeKey_Power, basePower);
+		attributes.SetBaseAttribute(AttributeKey::AttributeKey_Toughness, baseToughness);
+		assert(attributes.GetCurrentAttribute(AttributeKey::AttributeKey_Power) == basePower);
+		assert(attributes.GetCurrentAttribute(AttributeKey::AttributeKey_Toughness) == baseToughness);
+		/*
+		* Walkthrough:
+		* AddLayeredEffect(Power, Add, 1, 2) calls updateCurrentAttributes() and results in currentAttributes[Power] == 2 + 1 == 3
+		* AddLayeredEffect(Toughness, Add, 1, 2) calls updateCurrentAttributes() and results in currentAttributes[Toughness] == 1 + 1 == 2
+		* AddLayeredEffect(Power, Set, 4, 0) calls recalculateCurrentAttributes() and results in [Power] == 4 + 1 == 5
+		* AddLayeredEffect(Power, Multiply, 2, 1) calls "eek a BUG!!!"
+		* 
+		* 
+		*/
+		std::vector<LayeredEffectDefinition> effects;
+		effects.push_back({ AttributeKey_Power, EffectOperation_Add, /*modifier*/1, /*layer*/2 });
+		effects.push_back({ AttributeKey_Toughness, EffectOperation_Add, /*modifier*/1, /*layer*/2 });
+		effects.push_back({ AttributeKey_Power, EffectOperation_Set, /*modifier*/basePower + 2, /*layer*/0 });
+		effects.push_back({ AttributeKey_Power, EffectOperation_Multiply, /*modifier*/2, /*layer*/1 });
+		effects.push_back({ AttributeKey_Power, EffectOperation_Subtract, /*modifier*/1, /*layer*/2 });
+
+		for (auto& effect : effects)
+		{
+			attributes.AddLayeredEffect(effect);
+		}
 	}
 };
