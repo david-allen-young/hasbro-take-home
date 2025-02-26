046af34b5994e1335e9e9e290e066ade12e46356
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-02-25 16:39:18 -0800

    Update LA_v3_Extension_Removals.hpp

--- LayeredEffectsProcessorMTG/LA_v3_Extension_Removals.hpp
+++ LayeredEffectsProcessorMTG/LA_v3_Extension_Removals.hpp
@@ -2,17 +2,27 @@
 #include "ILayeredAttributes.hpp"
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <array>
 
 class LA_v3_Extension_Removals : public ILayeredAttributes
 {
 public:
+	struct Mod
+	{
+		EffectOperation operation;
+		int modifier;
+		size_t uid;
+	};
+
 	LA_v3_Extension_Removals(bool errorLoggingEnabled = false, bool errorHandlingEnabled = false, size_t rereservationSize = 10ULL);
 	virtual ~LA_v3_Extension_Removals() = default;
 	void SetBaseAttribute(AttributeKey attribute, int value) override;
 	int GetCurrentAttribute(AttributeKey attribute) const override;
 	void AddLayeredEffect(LayeredEffectDefinition effect) override;
 	void ClearLayeredEffects() override;
+
+	bool RemoveLayeredEffect(size_t uid);
 
 private:
 	bool errorLoggingEnabled;
@@ -22,12 +32,18 @@ private:
 	static const size_t NumAttributes = AttributeKey::AttributeKey_Controller + 1;
 	std::array<int, NumAttributes> baseAttributes, currentAttributes, highestLayers;
 
-	struct Mod
+	struct ModIndex
 	{
-		EffectOperation operation;
-		int modifier;
+		int layer = -1;
+		size_t key = -1;
+		AttributeKey attribute;
 	};
-	using LayerModsMap = std::map</*layer*/int, std::vector<Mod>>;
+	std::unordered_map<size_t/*uid*/, ModIndex> modIndexes;
+	size_t currentUID = 0;
+	size_t createUID();
+
+	//using LayerModsMap = std::map</*layer*/int, std::vector<Mod>>;
+	using LayerModsMap = std::map</*layer*/int, std::map</*key*/int, Mod>>;
 	std::array<LayerModsMap, NumAttributes> attributeModifiers;
 
 	void logError(LayeredEffectDefinition effect);
