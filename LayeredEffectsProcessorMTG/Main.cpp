ff57edcc131686fdef0d8afda7eb00b50e5727eb
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-03-12 13:03:24 -0700

    Update Main.cpp

--- LayeredEffectsProcessorMTG/Main.cpp
+++ LayeredEffectsProcessorMTG/Main.cpp
@@ -11,15 +11,6 @@ int main()
 	LayeredAttributesUnitTests_v2 tests;
 	tests.runOperationalTests();
 	tests.runCrashTests(); 
-
-	//sandbox stuff
-	//std::vector<std::tuple<int, int, int, int>> effects;
-	//effects.push_back({ /*layer*/7, /*timestamp*/1, /*attribute*/ 1, /*modifier*/ 1});
-	//effects.push_back({ /*layer*/6, /*timestamp*/2, /*attribute*/ 1, /*modifier*/ 1});
-	//std::sort(effects.begin(), effects.end());
-	//int layer = std::get<0>(effects[0]);
-	//std::cout << "first layer == " << layer << std::endl;
-
 	return 0;
 }
 
